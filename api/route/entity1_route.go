91099b3d34af50d63c43a2d447f8b2bafb3f1788
Alejo <alejocarobolante@gmail.com>
2025-11-12 09:57:44 -0300

    Initial commit

---
+++ api/route/entity1_route.go
@@ -0,0 +1,14 @@
+package route
+
+import (
+	"gorm-template/bootstrap"
+	"time"
+
+	//	"gorm-template/api/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewEntityRouter(env *bootstrap.Env, timeout time.Duration, group *gin.RouterGroup) {
+	//ec := &controller.Entity1{}
+}
